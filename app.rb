974b1633c06aefffcb90e849dc5b7088d21189cd
Sharmina Akhtar <sakht318@gmail.com>
2018-03-11 12:23:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+   configure do
+     enable :sessions
+     set :session_secret, "checkout"
+   end
+
+    get '/' do
+      erb :index
+    end
+
+    post '/checkout' do
+      @session = params[:item][:name]
+
+      erb :checkout
+    end
+
 end
